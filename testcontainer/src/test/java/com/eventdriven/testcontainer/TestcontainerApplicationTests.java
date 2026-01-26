80dc2a4f1fbc8524f26d4f0502875be88d5e87df
persolenom <personenom@gmail.com>
2026-01-26 16:06:04 +0100

    aggiunto properties di test

--- testcontainer/src/test/java/com/eventdriven/testcontainer/TestcontainerApplicationTests.java
+++ testcontainer/src/test/java/com/eventdriven/testcontainer/TestcontainerApplicationTests.java
@@ -1,15 +1,11 @@
 package com.eventdriven.testcontainer;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 
 @Import(TestcontainersConfiguration.class)
 @SpringBootTest
-class TestcontainerApplicationTests {
+public class TestcontainerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
 
 }
